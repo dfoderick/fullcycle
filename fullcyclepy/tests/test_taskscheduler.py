82fbc91a133381b38d8f41aba444e2619ec74570
Dave Foderick <dfoderick@gmail.com>
2018-04-23 09:53:10 -0700

    tests to make sure task.interval=0 means disabled

---
+++ fullcyclepy/tests/test_taskscheduler.py
@@ -0,0 +1,21 @@
+import unittest
+from helpers.taskschedule import TaskSchedule
+
+class Test_test_taskscheduler(unittest.TestCase):
+    def test_task_scheduler_disabled(self):
+        task = TaskSchedule()
+        task.interval = 0
+        self.assertFalse(task.is_time_to_run())
+
+    def test_task_scheduler_runinit(self):
+        task = TaskSchedule(run_on_init=True)
+        task.interval = 0
+        self.assertFalse(task.is_time_to_run())
+
+    def test_task_scheduler_enabled(self):
+        task = TaskSchedule(run_on_init=True)
+        task.interval = 1
+        self.assertTrue(task.is_time_to_run())
+
+if __name__ == '__main__':
+    unittest.main()
