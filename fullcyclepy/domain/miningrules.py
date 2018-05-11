8974fc825e5b2405e5503af40b018c31375fce01
Dave Foderick <dfoderick@gmail.com>
2018-05-10 22:32:03 -0700

    added new properties to domain

--- fullcyclepy/domain/miningrules.py
+++ fullcyclepy/domain/miningrules.py
@@ -3,12 +3,13 @@ from domain.mining import Miner, MinerStatistics
 
 class RuleParameters(object):
     '''configurable parameters for rules'''
-    def __init__(self, minertype, hashlimit, controllertemplimit, boardtemplimit, restartaftersec):
+    def __init__(self, minertype, hashlimit, controllertemplimit, boardtemplimit, restartaftersec, maxtempreset = None):
         self.minertype = minertype
         self.hashlimit = hashlimit
         self.controllertemplimit = controllertemplimit
         self.boardtemplimit = boardtemplimit
         self.restartaftersec = restartaftersec
+        self.maxtempreset = maxtempreset
 
 class BrokenRule(object):
     '''broken rules'''
@@ -48,21 +49,39 @@ class MinerStatisticsRule(object):
             return True
         return False
 
+    def has_reboot_rule(self):
+        return any(therule.action == 'restart' and therule.parameter == 'reboot' for therule in self.brokenrules)
+
+    def addbrokenrule(self, rule):
+        if rule.action == 'restart':
+            if not self.has_reboot_rule():
+                self.brokenrules = [therule for therule in self.brokenrules if therule.action != 'restart']
+                self.brokenrules.append(rule)
+        else:
+            self.brokenrules.append(rule)
+
     def isbroken(self):
         '''true when the rule is broken'''
         self.brokenrules = []
         if self.miner.miner_type != self.ruleparameters.minertype:
             return False
+
         if self.hasreading_num(self.statistics.currenthash):
             if self.statistics.currenthash < self.ruleparameters.hashlimit:
                 self.brokenrules.append(BrokenRule(self.miner, 'alert', 'on {0} low hash {1} below {2}'.format(self.miner.name, self.statistics.currenthash, self.ruleparameters.hashlimit)))
                 if self.statistics.elapsed > self.ruleparameters.restartaftersec:
-                    self.brokenrules.append(BrokenRule(self.miner, 'restart', 'restart'))
-                    self.brokenrules.append(BrokenRule(self.miner, 'alert', 'restarting {0} '.format(self.miner.name)))
+                    self.addbrokenrule(BrokenRule(self.miner, 'restart', 'restart'))
+                    self.addbrokenrule(BrokenRule(self.miner, 'alert', 'restarting {0} '.format(self.miner.name)))
+
         if self.hasreading_num(self.statistics.controllertemp):
             if self.statistics.controllertemp and self.statistics.controllertemp > self.ruleparameters.controllertemplimit:
-                self.brokenrules.append(BrokenRule(self.miner, 'alert', 'on {0} controller temp {1} exceeded {2}'.format(self.miner.name, self.statistics.controllertemp, self.ruleparameters.controllertemplimit)))
+                self.addbrokenrule(BrokenRule(self.miner, 'alert', 'on {0} controller temp {1} exceeded {2}'.format(self.miner.name, self.statistics.controllertemp, self.ruleparameters.controllertemplimit)))
+
         if self.hasreading_num(self.statistics.tempboardmax()):
             if self.statistics.tempboardmax() > self.ruleparameters.boardtemplimit:
-                self.brokenrules.append(BrokenRule(self.miner, 'alert', 'on {0} board temp {1} exceeded {2}'.format(self.miner.name, self.statistics.tempboardmax(), self.ruleparameters.boardtemplimit)))
+                self.addbrokenrule(BrokenRule(self.miner, 'alert', 'on {0} board temp {1} exceeded {2}'.format(self.miner.name, self.statistics.tempboardmax(), self.ruleparameters.boardtemplimit)))
+            if self.ruleparameters.maxtempreset and self.statistics.tempboardmax() > self.ruleparameters.maxtempreset:
+                if self.statistics.elapsed > self.ruleparameters.restartaftersec:
+                    self.addbrokenrule(BrokenRule(self.miner, 'restart', 'reboot'))
+
         return len(self.brokenrules) > 0
--- fullcyclepy/tests/test_rules.py
+++ fullcyclepy/tests/test_rules.py
@@ -2,7 +2,7 @@
 import unittest
 import datetime
 from domain.mining import Miner, MinerInfo, MinerStatistics
-from domain.miningrules import RuleParameters, MinerStatisticsRule
+from domain.miningrules import RuleParameters, MinerStatisticsRule, BrokenRule
 
 class TestRules(unittest.TestCase):
     def setUp(self):
@@ -38,15 +38,56 @@ class TestRules(unittest.TestCase):
         self.assertTrue(isbroken)
         self.assertTrue(len(rules.brokenrules) > 0)
 
-    def test_low_temp(self):
-        '''test when temp is low on miner'''
-        self.minerstats.controllertemp = 101
+    def test_board_high_temp(self):
+        '''test when controller temp is high on miner'''
+        self.minerstats.tempboard1 = 101
         params = RuleParameters(self.miner.minerinfo.miner_type, 10000, 40, 55, 60*10)
         rules = MinerStatisticsRule(self.miner, self.minerstats, params)
 
         isbroken = rules.isbroken()
         self.assertTrue(isbroken)
         self.assertTrue(len(rules.brokenrules) > 0)
+
+    def test_board_high_temp_reset(self):
+        '''test when controller temp is high on miner'''
+        self.minerstats.tempboard1 = 150
+        self.minerstats.elapsed = (60 * 10) + 1
+        params = RuleParameters(self.miner.minerinfo.miner_type, 10000, 40, 55, 60*10, maxtempreset=120)
+        rules = MinerStatisticsRule(self.miner, self.minerstats, params)
+
+        isbroken = rules.isbroken()
+        self.assertTrue(isbroken)
+        self.assertTrue(rules.has_reboot_rule())
+
+    def test_controller_high_temp_reset_null(self):
+        '''test when controller temp is high on miner. no reset'''
+        self.minerstats.controllertemp = 101
+        params = RuleParameters(self.miner.minerinfo.miner_type, 10000, 40, 55, 60*10, maxtempreset = None)
+        rules = MinerStatisticsRule(self.miner, self.minerstats, params)
+
+        isbroken = rules.isbroken()
+        self.assertTrue(isbroken)
+        self.assertFalse(rules.has_reboot_rule())
+
+
+    def test_only_one_reset(self):
+        rules = MinerStatisticsRule(self.miner, None, None)
+        rules.addbrokenrule(BrokenRule(self.miner,'restart',''))
+        rules.addbrokenrule(BrokenRule(self.miner,'restart',''))
+        self.assertTrue(len(rules.brokenrules) == 1)
+
+    def test_only_one_reboot(self):
+        rules = MinerStatisticsRule(self.miner, None, None)
+        rules.addbrokenrule(BrokenRule(self.miner,'restart','reboot'))
+        rules.addbrokenrule(BrokenRule(self.miner,'restart','reboot'))
+        self.assertTrue(len(rules.brokenrules) == 1)
+
+    def test_reset_leaves_reboot(self):
+        rules = MinerStatisticsRule(self.miner, None, None)
+        rules.addbrokenrule(BrokenRule(self.miner,'restart','reboot'))
+        rules.addbrokenrule(BrokenRule(self.miner,'restart',''))
+        self.assertTrue(len(rules.brokenrules) == 1)
+        self.assertTrue(rules.brokenrules[0].parameter == 'reboot')
 
 if __name__ == '__main__':
     unittest.main()
