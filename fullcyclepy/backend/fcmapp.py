7de6a0e68e0f0ece34ec118a58221e0121b2d347
Dave Foderick <dfoderick@gmail.com>
2018-09-09 22:32:04 -0700

    put bus into separate file

--- fullcyclepy/backend/fcmapp.py
+++ fullcyclepy/backend/fcmapp.py
@@ -6,9 +6,9 @@ import datetime
 import logging
 import json
 import base64
+import redis
 from collections import defaultdict
 from colorama import init, Fore
-import pika
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from domain.mining import Miner, Pool, MinerPool, AvailablePool, MinerCurrentPool, MinerStatistics, MinerStatus
@@ -24,6 +24,7 @@ from helpers.antminerhelper import setminertoprivileged, privileged, setprivileg
 from helpers.temperaturehelper import readtemperature
 from helpers.telegramhelper import sendalert, sendphoto
 from backend.fcmcache import Cache, CacheKeys
+from backend.fcmbus import Bus
 
 class ComponentName:
     '''names of components, corresponds to queue login names'''
@@ -98,89 +99,6 @@ class Antminer():
 
     def set_frequency(self, miner, frequency):
         return set_frequency(miner, self.__login, frequency)
-
-
-class Bus:
-    _connection = None
-    _durable = False
-
-    def __init__(self, servicelogin):
-        self._servicelogin = servicelogin
-        self._userlogin = self._servicelogin.user
-        if self._userlogin is None:
-            self._userlogin = 'fullcycle'
-
-    def connection(self):
-        if not self._connection:
-            credentials = pika.PlainCredentials(self._userlogin, self._servicelogin.password)
-            parameters = pika.ConnectionParameters(self._servicelogin.host, self._servicelogin.port, '/', credentials)
-            self._connection = pika.BlockingConnection(parameters)
-        return self._connection
-
-    def close(self):
-        if self._connection:
-            self._connection.close()
-
-    def sleep(self, seconds):
-        if self._connection:
-            self._connection.sleep(seconds)
-
-    def get_queue_name(self, queue_name):
-        name_of_q = queue_name
-        if isinstance(queue_name, QueueName):
-            name_of_q = QueueName.value(queue_name)
-        return name_of_q
-
-    def publish(self, queue_name, msg, exchange=''):
-        """Publishes message on new channel"""
-        localchannel = self.connection().channel()
-        localchannel.queue_declare(queue=self.get_queue_name(queue_name), durable=self._durable)
-        localchannel.basic_publish(exchange=exchange, routing_key=name_of_q, body=msg)
-        localchannel.close()
-
-    def broadcast(self, exchange_name, msg):
-        '''broadcast a message to anyone that is listening'''
-        localchannel = self.connection().channel()
-        localchannel.exchange_declare(exchange=self.get_queue_name(exchange_name), exchange_type='fanout')
-        localchannel.basic_publish(exchange=self.get_queue_name(exchange_name), routing_key='', body=msg)
-        localchannel.close()
-
-    def subscribe(self, name, callback, no_acknowledge=True, prefetch_count=1):
-        """basic subscribe messages from one queue
-        remember to listen to channel to get messages
-        """
-        localchannel = self.connection().channel()
-        localchannel.queue_declare(queue=self.get_queue_name(name))
-        localchannel.basic_qos(prefetch_count=prefetch_count)
-        localchannel.basic_consume(callback, queue=self.get_queue_name(name), no_ack=no_acknowledge)
-        return localchannel
-
-    def subscribe_broadcast(self, name, callback, no_acknowledge=True, prefetch_count=1):
-        """Consumes messages from one queue"""
-        localchannel = self.connection().channel()
-        localchannel.exchange_declare(exchange=self.get_queue_name(name), exchange_type='fanout')
-
-        result = localchannel.queue_declare(exclusive=True)
-        queue_name = result.method.queue
-        localchannel.queue_bind(exchange=self.get_queue_name(name), queue=queue_name)
-
-        localchannel.basic_qos(prefetch_count=prefetch_count)
-        localchannel.basic_consume(callback, queue=queue_name, no_ack=no_acknowledge)
-        return localchannel
-
-    def listen(self, channel):
-        '''listen to queue. this is a blocking call'''
-        channel.start_consuming()
-
-    def acknowledge(self, channel, delivery_tag):
-        '''acknowledge that message was processed'''
-        channel.basic_ack(delivery_tag)
-
-    def reject(self, channel, delivery_tag):
-        '''tell queue that message was not processed'''
-        channel.basic_nack(delivery_tag)
-
-
 
 class ApplicationService:
     '''Application Services'''
--- fullcyclepy/tests/test_bus.py
+++ fullcyclepy/tests/test_bus.py
@@ -1,14 +1,17 @@
 import unittest
 from helpers.queuehelper import QueueName
-from backend.fcmapp import Bus, InfrastructureService
+from backend.fcmapp import InfrastructureService
+from backend.fcmbus import Bus
 
 class Test_bus(unittest.TestCase):
+    def make_bus(self):
+        return Bus(InfrastructureService('', '', '', '', '', ''))
     def test_bus_get_name_q(self):
-        bus = Bus(InfrastructureService('','','','','',''))
+        bus = self.make_bus()
         self.assertTrue(bus.get_queue_name(QueueName.Q_ALERT) == "alert")
 
     def test_bus_get_name_str(self):
-        bus = Bus(InfrastructureService('','','','','',''))
+        bus = self.make_bus()
         self.assertTrue(bus.get_queue_name("alert") == "alert")
 
 if __name__ == '__main__':
