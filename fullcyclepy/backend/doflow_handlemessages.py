0c0002455eaea58bdbccfa6c420b18ce05ed90e4
Dave Foderick <dfoderick@gmail.com>
2018-09-12 11:26:12 -0700

    use method dispatch table instead of case

--- fullcyclepy/backend/doflow_handlemessages.py
+++ fullcyclepy/backend/doflow_handlemessages.py
@@ -9,50 +9,65 @@ from backend.when_offline import dooffline
 from backend.when_online import doonline
 from backend.when_restart import dorestart
 
-def is_queue_match(queue_name, queue_enum):
-    return queue_name == queue_enum or queue_name == QueueName.value(queue_enum)
+#def is_queue_match(queue_name, queue_enum):
+#    return queue_name == queue_enum or queue_name == QueueName.value(queue_enum)
 
 def dispatchmessages(mainapp, entries):
     '''process the messages'''
     if entries is None:
         return
+
+    dispatch_table = {
+        QueueName.value(QueueName.Q_DISCOVERED): run_discovered,
+        QueueName.value(QueueName.Q_ALERT): run_alert,
+        QueueName.value(QueueName.Q_PROVISION): run_provision,
+        QueueName.value(QueueName.Q_MONITORMINER): run_monitorminer,
+        QueueName.value(QueueName.Q_STATISTICSUPDATED): run_statsupdated,
+        QueueName.value(QueueName.Q_OFFLINE): run_offline,
+        QueueName.value(QueueName.Q_ONLINE): run_online,
+        QueueName.value(QueueName.Q_RESTART): run_restart,
+        }
+
     for entry in entries.entries:
-
-        if is_queue_match(entry.queuename, QueueName.Q_DISCOVERED):
-            miner = mainapp.messagedecodeminer(entry.message)
-            more = dodiscovered(miner)
-            dispatchmessages(mainapp, more)
-
-        elif is_queue_match(entry.queuename, QueueName.Q_ALERT):
-            doalert(entry.message)
-
-        elif is_queue_match(entry.queuename, QueueName.Q_PROVISION):
-            miner = mainapp.messagedecodeminer(entry.message)
-            more = doprovision(miner)
-            dispatchmessages(mainapp, more)
-
-        elif is_queue_match(entry.queuename, QueueName.Q_MONITORMINER):
-            miner = mainapp.messagedecodeminer(entry.message)
-            more = domonitorminer(miner)
-            dispatchmessages(mainapp, more)
-
-        elif is_queue_match(entry.queuename, QueueName.Q_STATISTICSUPDATED):
-            msg = mainapp.messagedecodeminerstats(entry.message)
-            more = dorules(msg.miner, msg.minerstats, msg.minerpool)
-            dispatchmessages(mainapp, more)
-
-        elif is_queue_match(entry.queuename, QueueName.Q_OFFLINE):
-            miner = mainapp.messagedecodeminer(entry.message)
-            more = dooffline(miner)
-            dispatchmessages(mainapp, more)
-
-        elif is_queue_match(entry.queuename, QueueName.Q_ONLINE):
-            miner = mainapp.messagedecodeminer(entry.message)
-            more = doonline(miner)
-            dispatchmessages(mainapp, more)
-
-        elif is_queue_match(entry.queuename, QueueName.Q_RESTART):
-            minermsg = mainapp.messagedecodeminercommand(entry.message)
-            dorestart(minermsg.miner, minermsg.command)
+        run_method = dispatch_table[QueueName.value(entry.queuename)]
+        if run_method:
+            run_method(mainapp, entry)
         else:
             print('{0} is not handled'.format(entry.queuename))
+
+def run_alert(mainapp, entry):
+    doalert(entry.message)
+
+def run_discovered(mainapp, entry):
+    miner = mainapp.messagedecodeminer(entry.message)
+    more = dodiscovered(miner)
+    dispatchmessages(mainapp, more)
+
+def run_provision(mainapp, entry):
+    miner = mainapp.messagedecodeminer(entry.message)
+    more = doprovision(miner)
+    dispatchmessages(mainapp, more)
+
+def run_monitorminer(mainapp, entry):
+    miner = mainapp.messagedecodeminer(entry.message)
+    more = domonitorminer(miner)
+    dispatchmessages(mainapp, more)
+
+def run_statsupdated(mainapp, entry):
+    msg = mainapp.messagedecodeminerstats(entry.message)
+    more = dorules(msg.miner, msg.minerstats, msg.minerpool)
+    dispatchmessages(mainapp, more)
+
+def run_offline(mainapp, entry):
+    miner = mainapp.messagedecodeminer(entry.message)
+    more = dooffline(miner)
+    dispatchmessages(mainapp, more)
+
+def run_online(mainapp, entry):
+    miner = mainapp.messagedecodeminer(entry.message)
+    more = doonline(miner)
+    dispatchmessages(mainapp, more)
+
+def run_restart(mainapp, entry):
+    minermsg = mainapp.messagedecodeminercommand(entry.message)
+    dorestart(minermsg.miner, minermsg.command)
